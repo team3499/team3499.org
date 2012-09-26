903f1302fe5b884d98a3208e52abf1d5c356b8be
unknown <jeremywfsbc@yahoo.com>
2012-09-26 14:59:54 -0400

    added comments but didnt finish yet - like buttons/links

--- phpfiles/home-content.php
+++ phpfiles/home-content.php
@@ -13,6 +13,20 @@
             <br />
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
+        <div class="postComments">
+          <div class="postCommentsComment">
+            <div class="postCommentsCommentUser">
+              <p><a href="#">ExampleUser</a><br />
+              Admin</p>
+              <div class="postCommentsCommentUserImage">
+                <img width="100px" src="/core/img/default.jpg" />
+              </div>
+            </div>
+            <div class="postCommentsCommentText">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
         <div class="postCata">
             <a href="#">Blog and Events</a>
         </div>
