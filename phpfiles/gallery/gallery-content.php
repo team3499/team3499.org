6fc056c1dff34f81e019c79776f08bf9a0f29790
unknown <jeremywfsbc@yahoo.com>
2012-10-01 20:43:39 -0400

    fixed images and resized - will add some scripts to make it look cool soon/later

--- phpfiles/gallery/gallery-content.php
+++ phpfiles/gallery/gallery-content.php
@@ -1,31 +1,36 @@
 <div class=galleryContent>
   <div class="galleryFeature">
-    <img width="100px" height="100px" src="/phpfiles/gallery/2012/1.jpg"/>
+    <a href="#"><img width="100%" height="100%" src="/core/img/2012/1.jpg"/></a>
     <div class="galleryYear">
-      <p>2012</p>
+      <a href="#">2012</a>
     </div>
   </div>
   <div class="galleryLink">
+  <a href="#"><img width="100%" height="100%" src="/core/img/2012/1.jpg"/></a>
      <div class="galleryYear">
-      <p>2011</p>
+      <a href="#">2011</a>
      </div>
   </div>
   <div class="galleryLink">
+  <a href="#"><img width="100%" height="100%" src="/core/img/2012/1.jpg"/></a>
     <div class="galleryYear">
-     <p> 3 </p>
+     <a href="#">3</a>
     </div>
   </div>
   <div class="galleryLink">
+  <a href="#"><img width="100%" height="100%" src="/core/img/2012/1.jpg"/></a>
     <div class="galleryYear">
      <p> 4 </p>
     </div>
   </div>
   <div class="galleryLink">
+  <a href="#"><img width="100%" height="100%" src="/core/img/2012/1.jpg"/></a>
     <div class="galleryYear">
      <p> 5 </p>
     </div>
   </div>
   <div class="galleryLink">
+  <a href="#"><img width="100%" height="100%" src="/core/img/2012/1.jpg"/></a>
     <div class="galleryYear">
      <p> 6 </p>
     </div>
