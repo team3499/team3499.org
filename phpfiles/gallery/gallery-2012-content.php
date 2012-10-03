2d4631a8aaac79fa9e8e61261122fdd064ce725d
unknown <jeremywfsbc@yahoo.com>
2012-10-02 21:24:34 -0400

    changed more styling with the gallery

--- phpfiles/gallery/gallery-2012-content.php
+++ phpfiles/gallery/gallery-2012-content.php
@@ -1,28 +1,28 @@
 <div class="galleryYearContent">
-  <table> <?php //add fade across when each image is loaded 1 by 1. ex: fade start on 1st img -> 2nd .... ?>
+  <table class="images"> <?php //add fade across when each image is loaded 1 by 1. ex: fade start on 1st img -> 2nd .... ?>
     <tr>
-      <td><a href="#"><img src="/core/img/2012/IMG_3863.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3868.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3893.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3874.JPG"/></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3863.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3868.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3893.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/default.jpg"/><span>tooltip test</span></a></td> <?php //can someone do an overflow thingy to make everything symmetrical- like in imgur/facebook albums images ?>
     </tr>
     <tr>
-      <td><a href="#"><img src="/core/img/2012/IMG_3863.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3868.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3893.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3874.JPG"/></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3863.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3868.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3893.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3874.JPG"/><span>tooltip test</span></a></td>
     </tr>
     <tr>
-      <td><a href="#"><img src="/core/img/2012/IMG_3863.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3868.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3893.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3874.JPG"/></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3863.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3868.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3893.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3874.JPG"/><span>tooltip test</span></a></td>
     </tr>
     <tr>
-      <td><a href="#"><img src="/core/img/2012/IMG_3863.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3868.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3893.JPG"/></a></td>
-      <td><a href="#"><img src="/core/img/2012/IMG_3874.JPG"/></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3863.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3868.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3893.JPG"/><span>tooltip test</span></a></td>
+      <td><a href="#" class="tooltip"><img src="/core/img/2012/IMG_3874.JPG"/><span>tooltip test</span></a></td>
     </tr>
   </table>
 
