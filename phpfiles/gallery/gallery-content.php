5b5a6665b5b73e1fba04376355eb4cb6fade7fc3
unknown <jeremywfsbc@yahoo.com>
2012-10-02 21:24:34 -0400

    changed more styling with the gallery

--- phpfiles/gallery/gallery-content.php
+++ phpfiles/gallery/gallery-content.php
@@ -1,7 +1,7 @@
 <div class=galleryContent>
 
   <div class="galleryFeature"> <?php //all images must be the same dimentions (16/10 i think) ?>
-    <a href="gallery2012" onclick="alert('fix fade out/in pl0x\n\n-alert on line 4, phpfiles/gallery/gallery-content');"><img width="100%" height="100%" src="/core/img/2012/1.jpg"/></a>
+    <a href="gallery2012" onclick="alert('fix fade out/in pl0x\n\n-alert on line 4, phpfiles/gallery/gallery-content');"><img src="/core/img/2012/1.jpg"/></a>
     <div class="galleryYear">
       <a href="gallery2012">2012</a>
     </div>
@@ -24,21 +24,21 @@
   <div class="galleryLink">
   <a href="#"><img src="/core/img/2012/1.jpg"/></a>
     <div class="galleryYear">
-     <p> 4 </p>
+     <a href="#">4</a>
     </div>
   </div>
   
   <div class="galleryLink">
   <a href="#"><img src="/core/img/2012/1.jpg"/></a>
     <div class="galleryYear">
-     <p> 5 </p>
+     <a href="#">5</a>
     </div>
   </div>
   
   <div class="galleryLink">
   <a href="#"><img src="/core/img/2012/1.jpg"/></a>
     <div class="galleryYear">
-     <p> 6 </p>
+     <a href="#">6</a>
     </div>
   </div>
 
